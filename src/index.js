03bbf6ae744a30bc3d7829ff781d2f2c69ad6f34
Mackenzie <mackbaird@outlook.com>
2024-05-06 00:16:32 +0100

    Moved JavaScript
    Moved JavaScript into separate folder

--- src/index.js
+++ src/index.js
@@ -1,23 +1,8 @@
-let edinburghTime = moment()
-  .tz("Europe/London")
-  .format("dddd, MMMM D, YYYY h:m A");
-
-let tampaTime = moment()
-  .tz("America/New_York")
-  .format("dddd, MMMM D, YYYY h:m A");
-
-let tokyoTime = moment().tz("Asia/Tokyo").format("dddd, MMMM D, YYYY h:m A");
-
 function showCityTime(event) {
-  if (event.target.value === "edinburgh") {
-    alert(`It is ${edinburghTime} in Edinburgh, UK`);
-  }
-  if (event.target.value === "tampa") {
-    alert(`It is ${tampaTime} in Tampa, FL USA`);
-  }
-  if (event.target.value === "tokyo") {
-    alert(`It is ${tokyoTime} in Tokyo, Japan`);
-  }
+  let currentTime = moment()
+    .tz(event.target.value)
+    .format("dddd, MMMM D, YYYY h:mm A");
+  alert(`It is ${currentTime} in ${event.target.value}`);
 }
 let citySelect = document.querySelector("#city-selector");
 citySelect.addEventListener("change", showCityTime);
