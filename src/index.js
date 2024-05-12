0008aefbc49d4a67514c05c72007349a026ead99
Mackenzie <mackbaird@outlook.com>
2024-05-12 20:40:52 +0100

    Live Times
    -Added and edited city names
    -Using Moment in JavaScript to give real time of cities across different time zones live

--- src/index.js
+++ src/index.js
@@ -6,3 +6,34 @@ function showCityTime(event) {
 }
 let citySelect = document.querySelector("#city-selector");
 citySelect.addEventListener("change", showCityTime);
+
+function updateTime() {
+  let newYorkElement = document.querySelector("#new-york");
+  let newYorkDateElement = newYorkElement.querySelector(".cityDate");
+  let newYorkTimeElement = newYorkElement.querySelector(".cityTime");
+  let newYorkTime = moment().tz("America/New_York");
+  newYorkDateElement.innerHTML = newYorkTime.format("MMMM Do YYYY");
+  newYorkTimeElement.innerHTML = newYorkTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+
+  let madridElement = document.querySelector("#madrid");
+  let madridDateElement = madridElement.querySelector(".cityDate");
+  let madridTimeElement = madridElement.querySelector(".cityTime");
+  let madridTime = moment().tz("Europe/Madrid");
+  madridDateElement.innerHTML = madridTime.format("MMMM Do YYYY");
+  madridTimeElement.innerHTML = madridTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+
+  let londonElement = document.querySelector("#london");
+  let londonDateElement = londonElement.querySelector(".cityDate");
+  let londonTimeElement = londonElement.querySelector(".cityTime");
+  let londonTime = moment().tz("Europe/London");
+  londonDateElement.innerHTML = londonTime.format("MMMM Do YYYY");
+  londonTimeElement.innerHTML = londonTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+}
+updateTime();
+setInterval(updateTime, 1000);
